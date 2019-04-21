cfd1eaa79ade889508c2d516b06f6f5c2f45dc7c
Nino Stephen Mathew <ninosm12@gmail.com>
2019-04-21 08:02:52 +0530

    Blockchain Module reconstruction. Glorified Linked List mode

--- blockchain.py
+++ blockchain.py
@@ -8,20 +8,61 @@ from json import dumps
 from hashlib import sha256
 from datetime import datetime
 from collections import OrderedDict
-from urllib.parse import urlparse
 from blockCrypto import verifyTransaction
 from blockCrypto import getPublicKey
 
+
+# Block Class
+class Block:
+    def __init__(self, blockNo = 0, data = "Genesis", previousRHash = '0x0', previousHash = '0x0'):
+        self.blockNo = blockNo
+        self.data = data
+        self.previousRHash = previousRHash
+        self.previousHash = previousHash
+        self.next = None
+
 class Blockchain:
-
     def __init__(self):
-        self.transactions = []
-        self.chain = []
-        self.nodes=()
-        self.nodeId = str(uuid4()).replace('-','')
-        #Create Genesis Block
-        self.createBlock("Genesis","00")
+        self.head = Block()
 
+    def addBlock(self, blockNo, data, previousRHash, previousHash):
+        newBlock = Block(blockNo, data, previousRHash, previousHash)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = newBlock
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next != None:
+            total = total + 1
+            cur = cur.next
+        print(total)
+        return total
+
+    def displayChain(self):
+        elems = []
+        cur = self.head
+        while cur.next != None:
+            elems.append(cur.blockNo)
+            cur = cur.next
+        elems.append(cur.blockNo)
+        print(elems)
+        return elems
+
+
+'''
+#class Blockchain:
+#
+#    def __init__(self):
+#        self.transactions = []
+#        self.chain = []
+#        self.nodes=()
+#        self.nodeId = str(uuid4()).replace('-','')
+#        #Create Genesis Block
+#        self.createBlock("Genesis","00")
+#
 #    def registerNode(self, nodeUrl):
 #        parsedUrl = urlparse(nodeUrl)
 #        if parsedUrl.netloc:
@@ -30,47 +71,48 @@ class Blockchain:
 #            self.nodes.add(parsedUrl.path)
 #        else :
 #            raise ValueError("Invalid Url")
-
-    def verify(self, username, r, s, transaction):
-        publicKey = getPublicKey(username)
-        return verifyTransaction(r, s, transaction, publicKey)
-
-    def addTransaction(self, senderAddr, recipientAddr, transactionData, r, s):
-        transaction = OrderedDict(
-            {
-                'senderAddr' : senderAddr,
-                'recipientAddr' : recipientAddr,
-                'data' : dumps(transactionData)
-            }
-        )
-        transaction = dumps(transaction)
-        print(transaction)
-        verified = verifyTransaction(r, s, transaction, senderpubKey=getPublicKey(username=senderAddr, type='user'))
-        if varified:
-            self.transactions.append(transaction)
-            return len(self.chain) + 1
-        return False
-
-    def createBlock(self, previousRHash, previousHash):
-        # hash(self.chain[-1])
-        block = {
-            'block number' : str(len(self.chain) + 1),
-            'timestamp' : datetime.now(),
-            'transactions' : dumps(self. transactions),
-            'previous related hash' : previousRHash,
-            'previous hash' : previousHash
-        }
-        self.transactions = []
-        self.chain.append(block)
-        return block
-
-    def hash(self, block):
-        blockString = dumps(block, sort_keys=True).encode()
-        return sha256(blockString).hexdigest()
-
-    def validateChain(self, chain):
-        for index in range(1, len(chain) + 1):
-            lastBlock = chain[index - 1]
-            currentBlock = chain[index]
-            if currentBlock['previousHash'] != self.hash(lastBlock):
-                return False
+#
+#    def verify(self, username, r, s, transaction):
+#        publicKey = getPublicKey(username)
+#        return verifyTransaction(r, s, transaction, publicKey)
+#
+#    def addTransaction(self, senderAddr, recipientAddr, transactionData, r, s):
+#        transaction = OrderedDict(
+#            {
+#                'senderAddr' : senderAddr,
+#                'recipientAddr' : recipientAddr,
+#                'data' : dumps(transactionData)
+#            }
+#        )
+#        transaction = dumps(transaction)
+#        print(transaction)
+#        verified = verifyTransaction(r, s, transaction, senderpubKey=getPublicKey(username=senderAddr, type='user'))
+#        if varified:
+#            self.transactions.append(transaction)
+#            return len(self.chain) + 1
+#        return False
+#
+#    def createBlock(self, previousRHash, previousHash):
+#        # hash(self.chain[-1])
+#        block = {
+#            'block number' : str(len(self.chain) + 1),
+#            'timestamp' : datetime.now(),
+#            'transactions' : dumps(self. transactions),
+#            'previous related hash' : previousRHash,
+#            'previous hash' : previousHash
+#        }
+#        self.transactions = []
+#        self.chain.append(block)
+#        return block
+#
+#    def hash(self, block):
+#        blockString = dumps(block, sort_keys=True).encode()
+#        return sha256(blockString).hexdigest()
+#
+#    def validateChain(self, chain):
+#        for index in range(1, len(chain) + 1):
+#            lastBlock = chain[index - 1]
+#            currentBlock = chain[index]
+#            if currentBlock['previousHash'] != self.hash(lastBlock):
+#                return False
+'''
