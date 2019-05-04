678ed79c80c4724935f52430c99e259dc60f0d78
Nino Stephen <ninosm12@gmail.com>
2019-05-04 20:50:23 +0530

    database designed

---
+++ database.sql
@@ -0,0 +1,20 @@
+#users table
+CREATE TABLE `egov`.`users` ( `userid` VARCHAR(70) NOT NULL PRIMARY KEY , `username` VARCHAR(30) NOT NULL UNIQUE, `name` VARCHAR(30) NOT NULL , `password` VARCHAR(300) NOT NULL , `registerationTime` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP );
+
+#keys table
+CREATE TABLE `egov`.`userKey` ( `sno` INT(4) UNSIGNED NOT NULL , `userid` VARCHAR(70) NOT NULL , `type` VARCHAR(10) NOT NULL , `publilcKey` VARCHAR(300) NOT NULL UNIQUE, `creation` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , `deprication` DATE NOT NULL );
+
+#Request table
+CREATE TABLE `egov`.`request` ( `requestID` VARCHAR(40) NOT NULL PRIMARY KEY, `userid` VARCHAR(70) NOT NULL , `unit` VARCHAR(10) NOT NULL , `subject` VARCHAR(50) NOT NULL , `body` TEXT NOT NULL , `submisionDate` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , `status` VARCHAR(10) NOT NULL , `comments` TEXT NOT NULL , `integrity` VARCHAR(10) NOT NULL);
+
+#Settings
+CREATE TABLE `egov`.`settings` ( `userid` VARCHAR(70) NOT NULL PRIMARY KEY, `phone` INT(10) NOT NULL UNIQUE , `email` VARCHAR(30) NOT NULL UNIQUE , `temporaryCode` INT(6) NOT NULL UNIQUE, `creation` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , `deprication` DATE NOT NULL );
+
+# Request status table
+CREATE TABLE `egov`.`requestStatus` ( `requestId` VARCHAR(20) NOT NULL PRIMARY KEY, `officialId` VARCHAR(70) NOT NULL , `action` TEXT NOT NULL , `actionTime` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP );
+
+# Nodes table
+CREATE TABLE `egov`.`nodes` ( `sno` INT(4) NOT NULL , `ip` VARCHAR(50) NOT NULL PRIMARY KEY, `status` VARCHAR(10) NOT NULL );
+
+#officials
+CREATE TABLE `egov`.`officials` ( `officialId` VARCHAR(70) NOT NULL PRIMARY KEY, `name` VARCHAR(50) NOT NULL , `unit` VARCHAR(10) NOT NULL , `email` VARCHAR(30) NOT NULL UNIQUE , `password` VARCHAR(300) NOT NULL , `secret` INT(6) NOT NULL UNIQUE, `registerationTime` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , `type` VARCHAR(10) NOT NULL, `grade` INT(1) NOT NULL ) ;
